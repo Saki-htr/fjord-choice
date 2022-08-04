3b3921465a56035ef2c6fa1f4c46444503f9fa4c
Saki-htr <saki.hattori0116@gmail.com>
2022-08-04 17:45:40 +0900

    bin/rails g model RawIssue raw_issue:jsonb

---
+++ app/models/raw_issue.rb
@@ -0,0 +1,2 @@
+class RawIssue < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_082102) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_084237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "raw_issues", force: :cascade do |t|
+    t.jsonb "raw_issue", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
