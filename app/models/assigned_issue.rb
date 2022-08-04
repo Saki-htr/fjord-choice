44059404207fb17a3910e1a38521171154eadd17
Saki-htr <saki.hattori0116@gmail.com>
2022-08-04 22:13:10 +0900

    rubocopにHasManyOrHasOneDependentを指摘されたため、dependentオプションを追記した

--- app/models/assigned_issue.rb
+++ app/models/assigned_issue.rb
@@ -3,6 +3,6 @@
 class AssignedIssue < ApplicationRecord
   belongs_to :raw_issue
 
-  has_many :user_assigned_issues
+  has_many :user_assigned_issues, dependent: :destroy
   has_many :users, through: :user_assigned_issues
 end
