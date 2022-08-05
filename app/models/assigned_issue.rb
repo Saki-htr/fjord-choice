87bb805270f1cf10de82deca0f1c3e95be16ce46
Saki-htr <saki.hattori0116@gmail.com>
2022-08-05 11:05:21 +0900

    1対1の関連付けを行った。生データの方がこのアプリではメインとなるため、AssignedIssueの方にhas_oneを付けた

--- app/models/assigned_issue.rb
+++ app/models/assigned_issue.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AssignedIssue < ApplicationRecord
-  belongs_to :raw_issue
+  has_one :raw_issue
 
   has_many :user_assigned_issues, dependent: :destroy
   has_many :users, through: :user_assigned_issues
--- app/models/raw_issue.rb
+++ app/models/raw_issue.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class RawIssue < ApplicationRecord
+  belongs_to :assigned_issue
 end
