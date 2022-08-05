f6f04d7fbc3c6ed3b9b6d86087a37a2cba7d8b21
Saki-htr <saki.hattori0116@gmail.com>
2022-08-05 11:10:14 +0900

    PRの生データのモデルと整形したデータのモデルの1対1の関連付けをした

--- app/models/raw_pull_request.rb
+++ app/models/raw_pull_request.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class RawPullRequest < ApplicationRecord
+  belongs_to :review_requested_pull_request
 end
--- app/models/review_requested_pull_request.rb
+++ app/models/review_requested_pull_request.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReviewRequestedPullRequest < ApplicationRecord
-  belongs_to :raw_pull_request
+  has_one :raw_pull_request
 
   has_many :user_pull_requests, dependent: :destroy
   has_many :users, through: :user_pull_requests
