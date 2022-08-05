2dbf079f12d072a17d3841d0c35f63b1f1d7a417
Saki-htr <saki.hattori0116@gmail.com>
2022-08-05 11:29:43 +0900

    関連付けしたテーブルを複数形に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_many :assigned_issue, through: :user_assigned_issues
 
   has_many :user_pull_requests, dependent: :destroy
-  has_many :review_requested_pull_request, through: :user_pull_requests
+  has_many :review_requested_pull_requests, through: :user_pull_requests
 end
