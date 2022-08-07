d9d9a33bb7deecd3598adf8fad648a90b6e67d92
Saki-htr <saki.hattori0116@gmail.com>
2022-08-07 10:33:51 +0900

    rubocopを通した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,11 @@ class HomeController < ApplicationController
     @users = User.all
 
     # ✅issue
-    issues = (1..5).map do |page| #500件取得
-      JSON.parse(Net::HTTP.get(URI.parse("https://api.github.com/repos/fjordllc/bootcamp/issues?state=all&sort=updated&per_page=100&page=#{page}")),symbolize_names: true)
+    issues = (1..5).map do |page| # 500件取得
+      JSON.parse(Net::HTTP.get(URI.parse("https://api.github.com/repos/fjordllc/bootcamp/issues?state=all&sort=updated&per_page=100&page=#{page}")),
+                 symbolize_names: true)
     end
-    issues.flatten! #1番外側の配列を削除
+    issues.flatten! # 1番外側の配列を削除
 
     # assigneeがnilの要素を削除
     issues.delete_if do |issue|
@@ -20,7 +21,7 @@ class HomeController < ApplicationController
     end
 
     # ✅PR
-    pulls_uri = URI.parse('https://api.github.com/repos/fjordllc/bootcamp/pulls?state=all&') #100件
+    pulls_uri = URI.parse('https://api.github.com/repos/fjordllc/bootcamp/pulls?state=all&') # 100件
     raw_pulls = Net::HTTP.get(pulls_uri)
     pulls = JSON.parse(raw_pulls, symbolize_names: true)
 
@@ -31,20 +32,17 @@ class HomeController < ApplicationController
 
     results = []
     @users.each do |user|
-
       issues_of_registered_users = issues.select do |issue|
         issue[:assignee][:login] == user.name
       end
 
-    # userがreviewerとして登録されているPRを、userごとに抽出する
-      #変数に入れることで、破壊的変更メソッドでなくてもselect後の状態を使える
+      # userがreviewerとして登録されているPRを、userごとに抽出する
+      # 変数に入れることで、破壊的変更メソッドでなくてもselect後の状態を使える
       pulls_of_registered_users = pulls.select do |pull|
         pull[:requested_reviewers][0][:login] == user.name
       end
-      pulls_of_registered_users = pulls_of_registered_users.to_json #もしかしたら .to_json しなくてもよしなにjson型にしてくれるかも?
+      pulls_of_registered_users = pulls_of_registered_users.to_json # もしかしたら .to_json しなくてもよしなにjson型にしてくれるかも?
       results << { user: user, assigned_issues: issues_of_registered_users, review_requested_pull_requests: pulls_of_registered_users }
     end
-    pp results[0]
   end
-  # この時点でresultsの中身は、
 end
