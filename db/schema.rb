f2f0836739c1183c775b30314c1c0ee025732bb5
Saki-htr <saki.hattori0116@gmail.com>
2022-08-14 18:23:16 +0900

    JSON生データを保存する2つのテーブルを、マイグレーションファイルでdrop_tableメソッドを使って削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_090142) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_14_091737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assigned_issues", force: :cascade do |t|
     t.integer "point", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "raw_issues", force: :cascade do |t|
-    t.jsonb "issue", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "raw_pull_requests", force: :cascade do |t|
-    t.jsonb "pull_request", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
