c494a8a84cf55862a1f84b4b11b3a05654910b9b
Saki-htr <saki.hattori0116@gmail.com>
2022-08-14 22:22:57 +0900

    AssignedIssueテーブルに、issueの番号を保存するnumberカラムを追加し、not null制約とユニーク制約を付けた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_100145) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_14_130709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_14_100145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "assignees", null: false, array: true
+    t.integer "number", null: false
     t.index ["assignees"], name: "index_assigned_issues_on_assignees"
+    t.index ["number"], name: "index_assigned_issues_on_number", unique: true
   end
 
   create_table "review_requested_pull_requests", force: :cascade do |t|
