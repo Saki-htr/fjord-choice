e08b2dc5035908fe4b5ec75f565def1074396012
Saki-htr <saki.hattori0116@gmail.com>
2022-08-14 22:53:06 +0900

    AssignedIssueテーブルにレコードをcreateする処理を追加した。updateは考慮していない。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,71 +7,92 @@ class HomeController < ApplicationController
   require 'octokit'
 
   def index
-    @users = User.all
-
     client = Octokit::Client.new \
       client_id: ENV['GITHUB_KEY'],
       client_secret: ENV['GITHUB_SECRET']
 
     # ✅ issue
-    issues = (1..3).map do |page|
-      client.list_issues('fjordllc/bootcamp', options = { state: 'all', sort: 'updated', per_page: 100, page: page })
+    issues = (1..1).map do |page|
+      client.search_issues('repo:fjordllc/bootcamp is:issue', options = { state: 'all', sort: 'updated', per_page: 3, page: page })
     end
     issues.flatten! # 1番外側の配列を削除
 
-    # assigneeがnilの要素を削除
-    issues.delete_if do |issue|
-      issue[:assignee].nil?
-    end
+    # AssignedIssue全削除
+    AssignedIssue.delete_all
 
-    # レスポンスに混ざっているPRの要素を削除
-    issues.delete_if do |issue|
-      issue.key?(:pull_request)
+    # AssignedIssueにレコードをCreateする
+    issues.each do |issue|
+      issue[:items].each do |i| #iの中身はhash
+        assigned_issue = AssignedIssue.new
+
+        # numberカラムの保存
+        assigned_issue.number = i[:number]
+
+        # pointカラムの保存
+        i[:labels].each do |label|
+          next if label[:name].to_i == 0 #ここで繰り返しを抜ける
+          #nextの条件がfalseの時だけ以下を実行
+          assigned_issue.point = label[:name].to_i
+        end
+
+        # assigneesの保存
+        if i[:assignees].empty?
+          assigned_issue.assignees = []
+        else
+          i[:assignees].each do |assignee|
+            assigned_issue.assignees = []
+            assigned_issue.assignees << assignee[:id]
+            assigned_issue.assignees
+          end
+        end
+
+        assigned_issue.save!
+      end
     end
 
     # ✅ PR
-    pulls = client.pull_requests('fjordllc/bootcamp', options = { state: 'open', per_page: 100 }) # openなPRしか取得しないので最大100件で充分
-    pulls.flatten!
+    # pulls = client.pull_requests('fjordllc/bootcamp', options = { state: 'open', per_page: 100 }) # openなPRしか取得しないので最大100件で充分
+    # pulls.flatten!
 
     # pull[:requested_reviewers][0][:login]がnilの要素を削除
-    pulls.delete_if do |pull|
-      pull[:requested_reviewers].empty?
-    end
+    # pulls.delete_if do |pull|
+    #   pull[:requested_reviewers].empty?
+    # end
 
-    results = []
-    @users.each do |user|
-      issues_of_registered_users = issues.select do |issue|
-        issue[:assignee][:login] == user.name
-      end
+    # results = []
+    # @users.each do |user|
+    #   issues_of_registered_users = issues.select do |issue|
+    #     issue[:assignee][:login] == user.name
+    #   end
 
       # userがreviewerとして登録されているPRを、userごとに抽出する
       # 変数に入れることで、破壊的変更メソッドでなくてもselect後の状態を使える
-      pulls_of_registered_users = pulls.select do |pull|
-        pull[:requested_reviewers][0][:login] == user.name
-      end
-      pulls_of_registered_users = pulls_of_registered_users.to_json # もしかしたら .to_json しなくてもよしなにjson型にしてくれるかも?
-      results << { user: user, assigned_issues: issues_of_registered_users, review_requested_pull_requests: pulls_of_registered_users }
-    end
+    #   pulls_of_registered_users = pulls.select do |pull|
+    #     pull[:requested_reviewers][0][:login] == user.name
+    #   end
+    #   pulls_of_registered_users = pulls_of_registered_users.to_json # もしかしたら .to_json しなくてもよしなにjson型にしてくれるかも?
+    #   results << { user: user, assigned_issues: issues_of_registered_users, review_requested_pull_requests: pulls_of_registered_users }
+    # end
 
     # raw_issueテーブルに保存
-    AssignedIssue.destroy_all
-    RawIssue.destroy_all #AssignedIssueと紐づいてるのでdeleteでもdestroyでも削除できない
+    # AssignedIssue.destroy_all
+    # RawIssue.destroy_all #AssignedIssueと紐づいてるのでdeleteでもdestroyでも削除できない
 
-    results.each do |result|
-      result[:assigned_issues].each do |i|
-        raw_issue = RawIssue.new
-        raw_issue.issue = i.to_hash.to_json
-        raw_issue.save!
+    # results.each do |result|
+    #   result[:assigned_issues].each do |i|
+    #     raw_issue = RawIssue.new
+    #     raw_issue.issue = i.to_hash.to_json
+    #     raw_issue.save!
 
-        i[:labels].each do |n|
-          next if n[:name].to_i == 0 #ここで繰り返しを抜ける
-          AssignedIssue.create!( #nextの条件がfalseの時だけ実行
-            point: n[:name].to_i,
-            raw_issue_id: raw_issue.id
-          )
-        end
-      end
-    end
+    #     i[:labels].each do |n|
+    #       next if n[:name].to_i == 0 #ここで繰り返しを抜ける
+    #       AssignedIssue.create!( #nextの条件がfalseの時だけ実行
+    #         point: n[:name].to_i,
+    #         raw_issue_id: raw_issue.id
+    #       )
+    #     end
+    #   end
+    # end
   end
 end
 
