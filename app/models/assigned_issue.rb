2b2f874bd5f280da76d34a3d678e0bb70ed456a8
Saki-htr <saki.hattori0116@gmail.com>
2022-08-14 18:29:28 +0900

    モデルの関連付けの記述を削除した

--- app/models/assigned_issue.rb
+++ app/models/assigned_issue.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class AssignedIssue < ApplicationRecord
-  belongs_to :raw_issue #関連付けをやめる?
-
   has_many :user_assigned_issues, dependent: :destroy
   has_many :users, through: :user_assigned_issues
 end
