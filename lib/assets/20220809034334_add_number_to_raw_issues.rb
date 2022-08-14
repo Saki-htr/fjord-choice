fad389b67a0f1dc02b8cfe53ebef833ad1c7236d
Saki-htr <saki.hattori0116@gmail.com>
2022-08-14 20:23:00 +0900

    rollback時に削除せず、念の為移動させていたマイグレーションファイルを削除した

--- lib/assets/20220809034334_add_number_to_raw_issues.rb
+++
@@ -1,7 +0,0 @@
-class AddNumberToRawIssues < ActiveRecord::Migration[7.0]
-  def change
-    add_column :raw_issues, :number, :integer
-    change_column_null :raw_issues, :number, false
-    add_index :raw_issues, :number, unique: true
-  end
-end
