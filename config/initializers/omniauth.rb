c9c959e8f65c63ca845453f2323e7f089d387f15
Saki-htr <saki.hattori0116@gmail.com>
2022-08-22 16:45:00 +0900

    herokuデプロイできないため、production環境の設定の書き方を変更した

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # development & test 環境に対して同じclient id&secretを設定する
   if Rails.env.development? || Rails.env.test?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] # ここでデータへのアクセスのscopeを指定できる
   else #production環境での設定
-    provider :github,
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
+    # provider :github,
+    #   Rails.application.credentials.github[:client_id],
+    #   Rails.application.credentials.github[:client_secret]
+    config.omniauth :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], scope: 'user,public_repo'
   end
 end
