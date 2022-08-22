1aefd1b8dedefc91beb18d0177f691b88a644e23
Saki-htr <saki.hattori0116@gmail.com>
2022-08-22 16:53:05 +0900

    変更を元に戻した

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'] # ここでデータへのアクセスのscopeを指定できる
   else #production環境での設定
-    # provider :github,
-    #   Rails.application.credentials.github[:client_id],
-    #   Rails.application.credentials.github[:client_secret]
-    config.omniauth :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret], scope: 'user,public_repo'
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret]
   end
 end
