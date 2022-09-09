3b49780a1729e87d69d779d6a15b2e13f8a71afc
Saki-htr <saki.hattori0116@gmail.com>
2022-09-09 12:47:45 +0900

    wheneverをアンインストールし、rakeタスクを削除

--- app/controllers/api/issues_controller.rb
+++ app/controllers/api/issues_controller.rb
@@ -1,22 +1,23 @@
 class Api::IssuesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-  # 読み込み時に一度パースが必要
-  # json_request = JSON.parse(request.body.read)
+    # 読み込み時に一度パースが必要
+    # json_request = JSON.parse(request.body.read)
 
-  # パース後のデータを表示
-  # p "json_request"
-  # p json_request
-  p "params"
-  p params
+    # パース後のデータを表示
+    # p "json_request"
+    # p json_request
+    p "params"
+    p params
 
-  assigned_issue = AssignedIssue. find_or_initialize_by(number: params[:number])
-  assigned_issue.update!(
-    number: params[:number],
-    point: params[:labels].map(&:to_i).sum,
-    assignees: params[:assignees].map(&:to_s)
-  )
-  assigned_issue.save!
+    assigned_issue = AssignedIssue. find_or_initialize_by(number: params[:number])
+    assigned_issue.update!(
+      number: params[:number],
+      point: params[:labels].map(&:to_i).sum,
+      assignees: params[:assignees].map(&:to_s)
+    )
+    assigned_issue.save!
+  end
 
   private
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  require 'uri'
-  require 'net/http'
   require 'json'
   require 'octokit'
 
 
   def index
-    @issues = AssignedIssue.all
-    @pulls = ReviewRequestedPullRequest.all
   end
 end
