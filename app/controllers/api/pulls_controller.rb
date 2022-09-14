7bd78fe830cb4fe751078f04f6ed45a8b0b3b6d5
Saki-htr <saki.hattori0116@gmail.com>
2022-09-14 19:11:51 +0900

    createアクション実行前に、送られてきたtokenパラメーターと、ENVファイルに設定したトークンが同一ならtrueとし、falseならunauthorizedを返すメソッドを追加した

--- app/controllers/api/pulls_controller.rb
+++ app/controllers/api/pulls_controller.rb
@@ -2,14 +2,25 @@
 
 class Api::PullsController < ApplicationController
   protect_from_forgery except: :create
+  before_action :require_token, only: [:create]
+
   def create
     review_requested_pr = ReviewRequestedPullRequest.find_or_initialize_by(number: params[:number])
     review_requested_pr.update!(pull_params)
     redirect_to root_path
   end
 
+  protected
+
+  def require_token
+    return if ENV['FJORD_CHOICE_TOKEN'] == params[:token]
+
+    head :unauthorized
+  end
+
   private
+
   def pull_params
-    params.require(:pull).permit(:title, :number, :state, :reviewers)
+    params.require(:pull).permit(:title, :number, :state ,:reviewers)
   end
 end
