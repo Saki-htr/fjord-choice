1a479e00458206a81292049d31bf9645a1ec83bd
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 18:16:53 +0900

    issueモデルのファイル名を変更した

--- app/models/assigned_issue.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Issue < ApplicationRecord
-  class << self
-    def total_points(user)
-      Issue.where("#{user.uid} = ANY(assignees)").sum(:point)
-    end
-  end
-end
