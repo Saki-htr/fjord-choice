b10713c69500d7f0a834cfdea1e6cc99a997b93c
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 17:56:59 +0900

    pendingになっているテストをコメントアウトした

--- spec/views/api/issues/create.html.slim_spec.rb
+++ spec/views/api/issues/create.html.slim_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe 'issues/create.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe 'issues/create.html.slim', type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/views/api/pulls/create.html.slim_spec.rb
+++ spec/views/api/pulls/create.html.slim_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe 'pulls/create.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe 'pulls/create.html.slim', type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
