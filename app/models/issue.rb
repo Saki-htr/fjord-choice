26641154f9f4bb7432d7f41131e5aa2e547ae0e9
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 17:17:11 +0900

    中間テーブル2つが必要なくなったので、削除した

---
+++ app/models/issue.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Issue < ApplicationRecord
+  has_many :user_assigned_issues, dependent: :destroy
+  has_many :users, through: :user_assigned_issues
+
+  class << self
+    def total_points(user)
+      AssignedIssue.where("#{user.uid} = ANY(assignees)").sum(:point)
+    end
+
+  end
+end
