781c028d77123786725f98d153e3d96c26ada8de
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 18:12:31 +0900

    不要な関連付けの記述を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :user_assigned_issues, dependent: :destroy
-  has_many :assigned_issues, through: :user_assigned_issues
-
-  has_many :user_pull_requests, dependent: :destroy
-  has_many :review_requested_pull_requests, through: :user_pull_requests
-
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid].to_i
