5a1f668395022977d994ec69bb138923b3e89b05
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 18:12:31 +0900

    不要な関連付けの記述を削除

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Issue < ApplicationRecord
-  has_many :user_assigned_issues, dependent: :destroy
-  has_many :users, through: :user_assigned_issues
-
   class << self
     def total_points(user)
       Issue.where("#{user.uid} = ANY(assignees)").sum(:point)
