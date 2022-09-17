d5165e0d2fb5caa2c5c6f7f22fea5957ed5fc79e
Saki-htr <saki.hattori0116@gmail.com>
2022-09-17 17:32:34 +0900

    関連付けの記述を削除し、ファイル名を変更した

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Issue < ApplicationRecord
+class AssignedIssue < ApplicationRecord
   has_many :user_assigned_issues, dependent: :destroy
   has_many :users, through: :user_assigned_issues
 
--- app/models/user_pull_request.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class UserPullRequest < ApplicationRecord
-  belongs_to :user
-  belongs_to :review_requested_pull_request
-end
