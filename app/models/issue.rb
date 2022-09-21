bc1a99d2ce081137d43adb7542982c013351cde1
Saki-htr <saki.hattori0116@gmail.com>
2022-09-21 22:55:50 +0900

    issueモデルにバリデーション追加

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Issue < ApplicationRecord
+  validates :number, presence: true, uniqueness: true
+  with_options presence: true do
+    validates :point
+    validates :assignees
+  end
 
   def self.total_points(user)
     Issue.where("#{user.uid} = ANY(assignees)").sum(:point)
