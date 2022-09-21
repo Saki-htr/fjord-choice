ebd2c2efbd890bfea89b73275af4f82120c6f01b
Saki-htr <saki.hattori0116@gmail.com>
2022-09-22 01:26:05 +0900

    PRのモデルテストを追加

---
+++ spec/factories/pull_requests.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :pull_request do
+    number { 1 }
+    title { 'openになっているPR' }
+    state { 'open' }
+    reviewers { [12_345_678] } # Aliceのuid
+  end
+
+  factory :pull_request2, class: PullRequest do
+    number { 2 }
+    title { 'closeされたPR' }
+    state { 'closed' }
+    reviewers { [12_345_678] } # Aliceのuid
+  end
+end
--- spec/models/pull_request_spec.rb
+++ spec/models/pull_request_spec.rb
@@ -2,6 +2,54 @@
 
 require 'rails_helper'
 
-# RSpec.describe PullRequest, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe PullRequest, type: :model do
+  describe '#validations' do
+    it 'ファクトリが有効であること' do
+      expect(build(:pull_request)).to be_valid
+    end
+
+    it 'numberが無ければ、レコード作成が無効であること' do
+      pull = build(:pull_request, number: nil)
+      pull.valid?
+      expect(pull.errors[:number]).to include("can't be blank")
+    end
+
+    it '重複したnumberが存在するなら、レコード作成が無効であること' do
+      create(:pull_request)
+      pull = build(:pull_request)
+      pull.valid?
+      expect(pull.errors[:number]).to include('has already been taken')
+    end
+
+    it 'titleが無ければ、レコード作成が無効であること' do
+      pull = build(:pull_request, title: nil)
+      pull.valid?
+      expect(pull.errors[:title]).to include("can't be blank")
+    end
+
+    it 'stateが無ければ、レコード作成が無効であること' do
+      pull = build(:pull_request, state: nil)
+      pull.valid?
+      expect(pull.errors[:state]).to include("can't be blank")
+    end
+
+    it 'reviewersが無ければ、レコード作成が無効であること' do
+      pull = build(:pull_request, reviewers: nil)
+      pull.valid?
+      expect(pull.errors[:reviewers]).to include("can't be blank")
+    end
+  end
+  describe '#review_requested' do
+    it 'ユーザーがレビュワーになっているプルリクエストを返すこと' do
+      user = create(:user)
+      pull = create(:pull_request, reviewers: [user.uid])
+      expect(PullRequest.review_requested(user)).to include(pull)
+    end
+
+    it 'ユーザーがレビュワーになっているが、ステータスがcloseのプルリクエストは返さないこと' do
+      user = create(:user)
+      pull = create(:pull_request2, reviewers: [user.uid])
+      expect(PullRequest.review_requested(user)).not_to include(pull)
+    end
+  end
+end
