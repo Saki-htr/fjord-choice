7e3d386ba019c06f16cceca2fcf1686e416d83fb
Saki-htr <saki.hattori0116@gmail.com>
2022-09-26 00:32:30 +0900

    リクエストスペックでletを使うように書き換えた

--- spec/requests/api/issues_spec.rb
+++ spec/requests/api/issues_spec.rb
@@ -3,58 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::Issues', type: :request do
-  describe 'POST /api/issues' do
-    context 'POSTされたissueと同じnumberのissueが、作成されていないとき' do
-      it 'issueを作成できること' do
+  let(:token) { ENV['FJORD_CHOICE_TOKEN'] }
+  let(:invalid_token) { '無効なトークン' }
+  let(:issue_params) do
+    { issue: FactoryBot.attributes_for(:issue, point: ['1']) }.to_json
+  end
+
+  let(:update_params) do
+    { issue: FactoryBot.attributes_for(:issue, point: ['2'], assignees: [23_456_789]) }.to_json
+  end
+
+  describe 'POSTされたイシューと同じnumberのイシューが作成されていない場合、イシューを新規作成する' do
+
+    context '無効なトークンが渡されたとき' do
+      it 'イシューを作成できないこと' do
+        post api_pulls_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(invalid_token)}, params: issue_params
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context '有効なトークンが渡されたとき' do
+      it 'イシューを作成できること' do
         expect do
-          post api_issues_path, params: {
-            number: 1,
-            labels: ['1'],
-            assignees: [12_345_678],
-            token: ENV['FJORD_CHOICE_TOKEN']
-          }
+          post api_issues_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(token)}, params: issue_params
         end.to change(Issue, :count).by(1)
         expect(response).to have_http_status(:created)
       end
     end
+  end
 
-    context 'POSTされたissueと同じnumberのissueが、既に作成されているとき' do
-      it 'Issueモデルのレコード件数が変化していないこと' do
-        create(:issue)
-        expect do
-          post api_issues_path, params: {
-            number: 1,
-            labels: ['2'],
-            assignees: [23_456_789],
-            token: ENV['FJORD_CHOICE_TOKEN']
-          }
-        end.not_to change(Issue, :count)
-      end
-
-      it '同じnumberのissueを更新できること' do
-        issue = create(:issue)
-        post api_issues_path, params: {
-          number: 1,
-          labels: ['2'],
-          assignees: [23_456_789],
-          token: ENV['FJORD_CHOICE_TOKEN']
-        }
-        expect(issue.reload.number).to eq 1
-        expect(issue.reload.point).to eq 2
-        expect(issue.reload.assignees).to eq [23_456_789]
-      end
-    end
+  describe 'POSTされたイシューと同じnumberのイシューが、既に作成されている場合、イシューを更新する' do
+    let!(:created_issue) { create(:issue) }
 
     context '無効なトークンが渡されたとき' do
-      it 'ステータスコード401を返すこと' do
-        post api_issues_path, params: {
-          number: 1,
-          labels: ['1'],
-          assignees: [12_345_678],
-          token: '無効なトークン'
-        }
+      it 'イシューを更新できないこと' do
+        post api_issues_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(invalid_token)}, params: issue_params
         expect(response).to have_http_status(:unauthorized)
       end
     end
+
+    context '有効なトークンが渡されたとき' do
+      it '同じnumberのイシューを更新できること' do
+        post api_issues_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(token)}, params: update_params
+        expect(created_issue.reload.number).to eq 1
+        expect(created_issue.reload.point).to eq 2
+        expect(created_issue.reload.assignees).to eq [23_456_789]
+      end
+
+      it 'Issueモデルのレコード件数が変化していないこと' do
+        expect do
+          post api_issues_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(token)}, params: update_params
+        end.not_to change(Issue, :count)
+      end
+    end
+  end
+
+  private
+
+  def authenticate_with_token(token)
+    ActionController::HttpAuthentication::Token.encode_credentials(token)
   end
 end
--- spec/requests/api/pulls_spec.rb
+++ spec/requests/api/pulls_spec.rb
@@ -3,26 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::Pulls', type: :request do
-  before do
-    @token = ENV['FJORD_CHOICE_TOKEN']
+  let(:token) { ENV['FJORD_CHOICE_TOKEN'] }
+  let(:invalid_token) { '無効なトークン' }
+  let(:pull_params) do
+    { pull: FactoryBot.attributes_for(:pull_request) }.to_json
+  end
+
+  let(:update_params) do
+    { pull: FactoryBot.attributes_for(:pull_request, title: '新しいタイトル', state: 'closed', reviewers: [23_456_789]) }.to_json
   end
 
   describe 'POSTされたプルリクエストと同じnumberのプルリクエストが作成されていない場合、プルリクエストを新規作成する' do
-    let(:headers) do
-      {
-        'Content-Type' => 'application/json',
-        'Authorization' => authenticate_with_token(@token)
-      }
-    end
-
-    let(:pull_params) do
-      { pull: FactoryBot.attributes_for(:pull_request) }.to_json
-    end
 
     context '無効なトークンが渡されたとき' do
       it 'プルリクエストを作成できないこと' do
-        headers['Authorization'] = authenticate_with_token('無効なトークン')
-        post api_pulls_path, headers: headers, params: pull_params
+        post api_pulls_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(invalid_token)}, params: pull_params
         expect(response).to have_http_status(:unauthorized)
       end
     end
@@ -30,47 +25,46 @@ RSpec.describe 'Api::Pulls', type: :request do
     context '有効なトークンが渡されたとき' do
       it 'プルリクエストを作成できること' do
         expect do
-          post api_pulls_path, headers: headers, params: pull_params
+          post api_pulls_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(token)}, params: pull_params
         end.to change(PullRequest, :count).by(1)
         expect(response).to have_http_status(:created)
       end
     end
 
-    describe 'POSTされたプルリクエストと同じnumberのプルリクエストが、既に作成されている場合、プルリクエストを更新する' do
-      let!(:created_pull_request) { FactoryBot.create(:pull_request) }
+  describe 'POSTされたプルリクエストと同じnumberのプルリクエストが、既に作成されている場合、プルリクエストを更新する' do
+    let!(:created_pull_request) { create(:pull_request) }
 
-      context '無効なトークンが渡されたとき' do
-        it 'PullRequestを更新できないこと' do
-          expect do
-            headers['Authorization'] = authenticate_with_token('無効なトークン')
-            post api_pulls_path, headers: headers, params: pull_params
-            expect(response).to have_http_status(:unauthorized)
-          end
-        end
-      end
-
-      context '有効なトークンが渡されたとき' do
-        it 'プルリクエストを更新できること' do
-          pull_params = { pull: FactoryBot.attributes_for(:pull_request, title: '新しいタイトル', state: 'closed', reviewers: [23_456_789]) }.to_json
-          post api_pulls_path, headers: headers, params: pull_params
-          expect(created_pull_request.reload.number).to eq 1
-          expect(created_pull_request.reload.title).to eq '新しいタイトル'
-          expect(created_pull_request.reload.state).to eq 'closed'
-          expect(created_pull_request.reload.reviewers).to eq [23_456_789]
-        end
-
-        it 'PullRequestモデルのレコード件数が変化していないこと' do
-          expect do
-            post api_pulls_path, headers: headers, params: pull_params
-          end.not_to change(Issue, :count)
+    context '無効なトークンが渡されたとき' do
+      it 'プルリクエストを更新できないこと' do
+        expect do
+          post api_pulls_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(invalid_token)}, params: pull_params
+          expect(response).to have_http_status(:unauthorized)
         end
       end
     end
 
-    private
+    context '有効なトークンが渡されたとき' do
+      it 'プルリクエストを更新できること' do
+        post api_pulls_path, headers: { 'Content-Type' => 'application/json', 'Authorization' => authenticate_with_token(token)}, params: update_params
 
-    def authenticate_with_token(token)
-      ActionController::HttpAuthentication::Token.encode_credentials(token)
+        expect(created_pull_request.reload.number).to eq 1
+        expect(created_pull_request.reload.title).to eq '新しいタイトル'
+        expect(created_pull_request.reload.state).to eq 'closed'
+        expect(created_pull_request.reload.reviewers).to eq [23_456_789]
+      end
+
+      it 'PullRequestモデルのレコード件数が変化していないこと' do
+        expect do
+          post api_pulls_path, headers: headers, params: update_params
+        end.not_to change(PullRequest, :count)
+      end
     end
+  end
+
+  private
+
+  def authenticate_with_token(token)
+    ActionController::HttpAuthentication::Token.encode_credentials(token)
+  end
   end
 end
