891e4d30e48018e2f30aa8a90d8a1f7aa791f34e
Saki-htr <saki.hattori0116@gmail.com>
2022-10-12 22:25:12 +0900

    sassc-railsのreadmeのInline Source Mapsの通りに、設定を記述した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # sassc-railsの設定
+  config.sass.inline_source_maps = true
 end
