45cb11e8f94823a0d3b2a5ad45b234931825695a
Saki-htr <saki.hattori0116@gmail.com>
2022-10-12 17:43:47 +0900

    linterの指摘を修正

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -1,44 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
   // モーダルを開いたり閉じたりする関数
-  function openModal($el) {
-    $el.classList.add('is-active');
+  function openModal ($el) {
+    $el.classList.add('is-active')
   }
 
-  function closeModal($el) {
-    $el.classList.remove('is-active');
+  function closeModal ($el) {
+    $el.classList.remove('is-active')
   }
 
-  function closeAllModals() {
+  function closeAllModals () {
     (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);
-    });
+      closeModal($modal)
+    })
   }
 
   //  特定のモーダルを開くためのボタンにクリックイベントを追加する
   (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-    const modal = $trigger.dataset.target;
-    const $target = document.getElementById(modal);
+    const modal = $trigger.dataset.target
+    const $target = document.getElementById(modal)
 
     $trigger.addEventListener('click', () => {
-      openModal($target);
-    });
+      openModal($target)
+    })
   });
 
   // 親のモーダルを閉じるために、さまざまな子要素にクリック イベントを追加する
   (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
+    const $target = $close.closest('.modal')
 
     $close.addEventListener('click', () => {
-      closeModal($target);
-    });
-  });
+      closeModal($target)
+    })
+  })
 
   // Aすべてのモーダルを閉じるためのキーボード イベントを追加する
   document.addEventListener('keydown', (event) => {
-    const e = event || window.event;
+    const e = event || window.event
 
     if (e.keyCode === 27) { // Escape key
-      closeAllModals();
+      closeAllModals()
     }
-  });
-});
+  })
+})
