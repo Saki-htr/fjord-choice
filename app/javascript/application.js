14538c2f65c4e9a36062a4d27707d2d6197bf0b9
Saki-htr <saki.hattori0116@gmail.com>
2022-10-19 17:31:51 +0900

    カスタムJSの読み込む位置を戻した

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import './custom/modal.js'
 import '@hotwired/turbo-rails'
 import 'controllers'
+import './custom/modal.js'
