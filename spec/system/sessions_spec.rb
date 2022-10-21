eef1c755e345bc4866d408b44a3842e695c35f2b
Saki-htr <saki.hattori0116@gmail.com>
2022-10-21 22:06:11 +0900

    ヘルパーに切り出したログイン処理を行うメソッドを使って、ログインできることを確認するテストを書いた

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe "Sessions", type: :system do
   describe 'ログイン処理' do
     context '認証が成功したとき' do
       it 'ログインできること' do
+        login_as(create(:user))
+        expect(page).to have_content('メンバーに追加されました')
       end
     end
 
     context '認証が失敗したとき' do
       it 'ログインできないこと' do
-
       end
     end
 
