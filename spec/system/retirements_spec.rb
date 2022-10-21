1f68ae153a0d07e48165bec5a399223a0fe360b3
Saki-htr <saki.hattori0116@gmail.com>
2022-10-21 23:12:02 +0900

    セッションとUserモデルのレコードを削除できることを検証するテストを追加した

--- spec/system/retirements_spec.rb
+++ spec/system/retirements_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Retirements", type: :system do
+  let(:user) { create(:user) }
 
   describe '退会処理' do
     context 'ログインしているとき' do
       it 'セッションとUserモデルのレコードを削除できること' do
-        accept_confirm { click_on 'Delete Account' }
-        expect(page).to have_content 'Successfully deleted your account!'
+        login_as user
+        click_button 'メンバーからはずれる'
+        expect do
+          expect(accept_confirm).to eq '本当にメンバーからはずれていいですか?'
+          expect(page).to have_content 'メンバーからはずれました'
+        end.to change { User.count }.by(-1)
       end
     end
   end
