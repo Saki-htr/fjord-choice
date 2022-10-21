fe5f94379143b9acea81f73880e017c0e7690f09
Saki-htr <saki.hattori0116@gmail.com>
2022-10-21 14:42:50 +0900

    JavaScriptがしようされている場合とされてない場合でドライバーを変更する意味が無いのと、ヘッドレスで実行した方が早いため、常にselenium_chrome_headlessで実行するようにした

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome
+    driven_by :selenium_chrome_headless
   end
 end
