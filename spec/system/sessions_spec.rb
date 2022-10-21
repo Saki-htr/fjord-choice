6031614c006459aef8d6225d713976270c90e66c
Saki-htr <saki.hattori0116@gmail.com>
2022-10-21 23:05:54 +0900

    今後テストが増えることを考慮し、使い回しそうな変数をletに切り出した

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :system do
+  let(:user) { create(:user) }
+  let(:invalid_user) { github_invalid_mock }
+
   describe 'ログイン処理' do
     context '認証が成功したとき' do
       it 'ログインできること' do
-        login_as(create(:user))
+        login_as user
         expect(page).to have_content('メンバーに追加されました')
       end
     end
     context '認証が失敗したとき' do
       it '認証せずトップページにリダイレクトすること' do
-        github_invalid_mock
+        invalid_user
         visit root_path
         click_button 'メンバーに自分を追加する'
         expect(page).to have_content('ログインに失敗しました')
