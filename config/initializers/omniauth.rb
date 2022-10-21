0cb97a2db6632aed84fab166f2869bcaffc3f12a
Saki-htr <saki.hattori0116@gmail.com>
2022-10-21 12:55:57 +0900

    認証まわりにつけていたコメントを削除した

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # development & test 環境に対して同じclient id&secretを設定する
   if Rails.env.development? || Rails.env.test?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'read:user'	 # ここでデータへのアクセスのscopeを指定できる
-  else #production環境での設定
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'read:user'
+  else
     provider :github,
       Rails.application.credentials.github[:client_id],
       Rails.application.credentials.github[:client_secret]
