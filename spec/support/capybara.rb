63b4483ddd7a2d0ae4a5924b3ffc68aed9822cff
Saki-htr <saki.hattori0116@gmail.com>
2022-10-22 15:12:05 +0900

    Selenium::WebDriver::Error::UnknownError解消のため、関係がありそうなgemをインストール

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,6 +5,6 @@ require_relative '../login_helper'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome
+    driven_by :selenium_chrome_headless
   end
 end
