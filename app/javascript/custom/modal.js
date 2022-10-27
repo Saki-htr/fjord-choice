d6dc85a44cb448cce746e162ac35b5bc2bed88cd
Saki-htr <saki.hattori0116@gmail.com>
2022-10-27 11:44:51 +0900

    clickをtouchendに変更(スマホで動作するか確認のため)

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = $trigger.dataset.target
     const $target = document.getElementById(modal)
 
-    $trigger.addEventListener('click', () => {
+    $trigger.addEventListener('touchend', () => {
       openModal($target)
     })
   });
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
   (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
     const $target = $close.closest('.modal')
 
-    $close.addEventListener('click', () => {
+    $close.addEventListener('touchend', () => {
       closeModal($target)
     })
   })
